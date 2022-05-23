b97582b60fe6041ab92c6198008e0ec49b95559e
Larissa Cristina Benedito <50052600+mewmewdevart@users.noreply.github.com>
2022-05-23 18:54:00 -0300

    update

--- Pdfs/LanguageC/cap4/ex0409.c
+++ Pdfs/LanguageC/cap4/ex0409.c
@@ -2,5 +2,18 @@
 
 void main ()
 {
-	printf("");
+	int linhas;
+	char ramos, galhos;
+
+	printf("Digite uma letra do alfabeto: ");
+	scanf("%c", &ramos);
+
+
+	for(galhos='A'; galhos<=ramos; galhos++)
+	{
+		for(linhas='A'; linhas<=galhos; linhas++)
+		printf("%c", ramos);
+
+		putchar('\n');
+	}
 }
