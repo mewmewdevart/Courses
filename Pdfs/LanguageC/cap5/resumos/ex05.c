ede84251edb2306efa550b19d78e08fdffb0e68c
Larissa Cristina Benedito <50052600+mewmewdevart@users.noreply.github.com>
2022-06-21 19:10:10 -0300

    to be continue

---
+++ Pdfs/LanguageC/cap5/resumos/ex05.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main ()
+{
+	/*
+		01- Indique se sao verdadeiras ou falsas as seguintes afirmacoes:
+			a) Uma função em C pode devolver simultaneamente mais do que um valor. [FALSO]
+			b) Uma função em C pode não ter parametros. [VERDADEIRO]
+			c) Uma função em C tem que devolver sempre um inteiro. [FALSO]
+			d) Os parametros das funções podem do tipo void. [FALSO]
+			e) A instrucao return termina a execução de uma função. [VERDADEIRO]
+			f) Uma variavel local a uma funcao pode ter o mesmo nome que um parametro. [FALSO]
+			g) A instrução return termina a execucao de uma funcao apenas se for a ultima instrucao da funcao em que se encontra.[FALSO]
+			h) A instrucao return, quando executada dentro de qualquer funcao, termina o programa.[FALSO]
+			i) A instrucao return, quando executada dentro da funcao main, termina o programa.[VERDADEIRO]
+			j) O nome de uma funcao é opcional. [FALSO]
+			k) Os parametros numa funcao sao opcionais. [VERDADEIRO]
+			l) Uma funcao deve fazer o maior numero de tarefas possivel sem ocupar muito codigo. [FALSO]
+			m) Uma funcao nao deve ter mais que 10 linhas [FALSO]
+			n) O nome de uma funcao nao deve ter mais de 6 letras [FALSO]
+			o) O nome de uma funcao nao pode ser uma palavra reservada do C. [VERDADEIRO]
+			p) Sempre que for necessario devem ser utilizadas variaveis locais. [VERDADEIRO]
+			q) Um prototipo nao é nada mais que a repetição do cabeçalho da função seguido de ; [VERDADEIRO]
+			r) Em C, um procedimento nao é mais do que uma funcao "retorna void")  [VERDADEIRO]
+	*/
+}
